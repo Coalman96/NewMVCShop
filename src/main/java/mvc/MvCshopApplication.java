a006684be75ab87f87070a1eb37c56768fa1b5cb
Coalman96 <mayuaa001@gmail.com>
2023-10-25 01:41:16 +0900

    test

--- src/main/java/mvc/MvCshopApplication.java
+++ src/main/java/mvc/MvCshopApplication.java
@@ -2,14 +2,20 @@ package mvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class MvCshopApplication {
+public class MvCshopApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(MvCshopApplication.class, args);
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MvCshopApplication.class);
     }
 
 }
