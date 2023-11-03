6dbb1e8752712dd0a044f40e114061481b250639
Coalman96 <mayuaa001@gmail.com>
2023-11-04 02:05:30 +0900

    test

--- src/main/java/mvc/MvCshopApplication.java
+++ src/main/java/mvc/MvCshopApplication.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -39,9 +40,9 @@ public class MvCshopApplication extends SpringBootServletInitializer implements 
             log.info("run");
         }
     }
-//    @Override
-//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//        return application.sources(MvCshopApplication.class);
-//    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MvCshopApplication.class);
+    }
 
 }
